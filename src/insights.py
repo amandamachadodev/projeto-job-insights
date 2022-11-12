b7fd18ccd871d2a2e775c3b39b74095bda05103e
mandioquynha <machado.amandag@gmail.com>
2022-11-12 18:39:27 -0300

    feat: :sparkles: implementada função get_max_salary
    requisito 4

--- src/insights.py
+++ src/insights.py
@@ -104,7 +104,14 @@ def get_max_salary(path):
     int
         The maximum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+    max_salary = 0
+    for salary in jobs:
+        if (salary["max_salary"].isnumeric() and int(salary["max_salary"]) > int(max_salary)):
+            max_salary = salary["max_salary"]
+        else:
+            max_salary = max_salary
+    return int(max_salary)
 
 
 def get_min_salary(path):
